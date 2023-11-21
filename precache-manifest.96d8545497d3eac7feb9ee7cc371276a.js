d3d88f95f44e4e42b7393c4106bcee70fe1122e6
martin <martin.corredor@rappi.com>
2023-11-21 12:50:20 -0500

    Updates

---
+++ precache-manifest.96d8545497d3eac7feb9ee7cc371276a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cf0130229cab6ab8638bd25b4107a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85f3f57e91c46bd6e4a6",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "465f080d36701ee2c4e2",
+    "url": "/static/js/2.64522f54.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.64522f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e096a6d02f204c4c42eb",
+    "url": "/static/js/3.8c523a3a.chunk.js"
+  },
+  {
+    "revision": "85f3f57e91c46bd6e4a6",
+    "url": "/static/js/main.05dc3283.chunk.js"
+  },
+  {
+    "revision": "ee9028c7074bcac16d00",
+    "url": "/static/js/runtime-main.ff99d937.js"
+  }
+]);
