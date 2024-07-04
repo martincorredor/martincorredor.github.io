53887b6e44e0ba14ca93940a2eeb4487ad2c04d7
Martin Corredor <dev.martincorredor@gmail.com>
2024-07-04 09:11:51 -0500

    [UPDATE] Change all UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import { useState } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Contact from './components/Contact';
-import Habilities from './components/Habilities';
-import Nav from './components/Nav';
-import Work from './components/Work';
-import About from './components/About';
-import NavMenu from './components/NavMenu';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Menu from './components/Menu';
 
 const App = () => {
   return (
-    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
-      <Header />
-      <Banner />
-      <Nav />
-      <About />
-      {/* <Habilities /> */}
-      <Work />
-      <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/habilities" element={<div>Habilidades</div>} />
+      </Routes>
+      <Menu/>
     </div>
   );
 };
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className='home-section'>
+      <div>Texto</div>
+      <div>Imagen</div>
+    </section>
+  );
+};
+
+export default Home;
