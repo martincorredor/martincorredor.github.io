8c5f5ba0d92f568c67f07ff1d37203c5e9adad68
Martin Corredor <dev.martincorredor@gmail.com>
2024-09-02 16:42:41 -0500

    [UPDATE] Clean code to start new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Menu from './components/Menu';
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/habilities" element={<div>Habilidades</div>} />
-      </Routes>
-      <Menu/>
+      Martin
     </div>
   );
 };
